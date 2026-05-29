406471136dbd8e808f755541eccf7ea780b3c532
atika <atika@biztwork.web.id>
2026-05-29 16:31:58 +0700

    Menambahkan beberapa fitur yang tidak lengkap

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -1,19 +1,56 @@
 ﻿Wizard wizardA = new Wizard("Jeruk", 5);
 Wizard wizardB = new Wizard("Apel", 15);
 
-Console.WriteLine("Permainan dimulai...\n");
+Console.WriteLine("FIGHT!");
 Console.WriteLine("Statistik Awal");
 wizardA.showStats();
 wizardB.showStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.Attack(wizardB);
+string Pilihan;
+while(true)
+{
+    Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} melakukan heal");
+    Console.WriteLine($"4. {wizardB.Name} melakukan heal");
 
-Console.WriteLine("Permainan selesai...\n");
-Console.WriteLine("Statistik Akhir");
-wizardA.showStats();
-wizardB.showStats();
+    Console.Write("\nPilihanmu (1/2/3/4): ");
+    Pilihan = Console.ReadLine();
+
+    if (Pilihan == "1") wizardA.Attack(wizardB);
+    else if (Pilihan == "2") wizardB.Attack(wizardA);
+    else if (Pilihan == "3") wizardA.Heal();
+    else if (Pilihan == "4") wizardB.Heal();
+    else Console.WriteLine("Pilihanmu tidak valid");
+
+    Console.Write("\nPilihanmu (1/2/3/4): ");
+    Pilihan = Console.ReadLine();
+
+    if (Pilihan == "1") wizardA.Attack(wizardB);
+    else if (Pilihan == "2") wizardB.Attack(wizardA);
+    else if (Pilihan == "3") wizardA.Heal();
+    else if (Pilihan == "4") wizardB.Heal();
+    else Console.WriteLine("Pilihanmu tidak valid");
+
+    if (wizardA.Energy <= 0 || wizardB.Energy <= 0)
+    {
+        Console.WriteLine("PERMAINAN BERAKHIR!!");
+
+        if (wizardA.Energy > wizardB.Energy)
+        {
+            Console.WriteLine($"{wizardB.Name} Berhasil dikalahkan!");
+            Console.WriteLine($"{wizardA.Name} Keluar sebagai pemenangnya!");
+        }
+        else
+        {
+            Console.WriteLine($"{wizardA.Name} Berhasil dikalahkan!");
+            Console.WriteLine($"{wizardB.Name} Keluar sebagai pemenangnya!");
+        }
+        break;
+    }
+    Console.WriteLine();
+
+}
 
 public class Wizard
 {
@@ -42,7 +79,26 @@ public class Wizard
         Console.WriteLine($"Energy       : {Energy}\n");
     }
 
-    
+    public void Heal()
+    {
+
+        if (Energy >= 100)
+        {
+            Console.WriteLine("Gagal melakukan Heal. Energy sudah mencapai maksimum!");
+        }
+        else
+        {
+            if (Energy > 95)
+            {
+                Energy = 100;
+            }
+            else
+            {
+                Energy += 5;
+            }
+            Console.WriteLine($"{Name} Berhasil melakukan Heal! Energy meningkat menjadi {Energy}");
+        }
+    }
 }
 
 
